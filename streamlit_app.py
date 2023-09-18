a9408d167ffe04f5828de62b19646ec0df2fea70
NuttyFlash <75692016+NuttyFlash@users.noreply.github.com>
2023-09-18 17:26:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -260,36 +260,6 @@ with tab2:
             except Exception as e:
                 st.exception(f"Exception: {e}")
 
-    # If 'Search & Summarize' button is clicked
-    if col2.button("Search & Summarize"):
-        # Validate inputs
-        if not openai_api_key or not serper_api_key:
-            st.error("Please provide the missing API keys in Settings.")
-        elif not search_query.strip():
-            st.error("Please provide the search query.")
-        else:
-            try:
-                with st.spinner("Please wait..."):
-                    # Show the top X relevant news articles from the previous week using Google Serper API
-                    search = GoogleSerperAPIWrapper(type="news", tbs="qdr:w1", serper_api_key=serper_api_key)
-                    result_dict = search.results(search_query)
-
-                    if not result_dict['news']:
-                        st.error(f"No search results for: {search_query}.")
-                    else:
-                        # Load URL data from the top X news search results
-                        for i, item in zip(range(num_results), result_dict['news']):
-                            loader = UnstructuredURLLoader(urls=[item['link']])
-                            data = loader.load()
-
-                            # Initialize the ChatOpenAI module, load and run the summarize chain
-                            llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo', openai_api_key=openai_api_key)
-                            chain = load_summarize_chain(llm, chain_type="map_reduce")
-                            summary = chain.run(data)
-
-                            st.success(f"Title: {item['title']}\n\nLink: {item['link']}\n\nSummary: {summary}")
-            except Exception as e:
-                st.exception(f"Exception: {e}")
 
         
 
